bfbcbffc2b065762df8731c043e3ff4e18e62cdc
mwauragitonga <mwauragitonga12@gmail.com>
2021-11-13 14:55:42 +0300

    Initial commit including login page

---
+++ assets/db/config.php
@@ -0,0 +1,9 @@
+<?php
+// Connection variables
+$dbhost	= "localhost";	   // localhost or IP
+$dbuser	= "root";		  // database username
+$dbpass	= "";		     // database password
+$dbname	= "notice_board";    // database name
+
+$mysqli = mysqli_connect($dbhost, $dbuser, $dbpass, $dbname); 
+?>
