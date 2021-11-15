bc28bd8da183a1fa1356193a78e60fce64eaabe9
mwauragitonga <mwauragitonga12@gmail.com>
2021-11-15 21:48:09 +0300

    db configs

--- config.php
+++ config.php
@@ -2,7 +2,7 @@
 
 function getConn(){
     // Connection variables
-    $dbhost	= "test76.interview.inisev.com";	   // localhost or IP
+    $dbhost	= "localhost";	   // localhost or IP
     $dbuser	= "kelv";		  // database username
     $dbpass	= "12345";		     // database password
     $dbname	= "notice_board";    // database name
